ffd645505c5f13286e294b078f66ef0d69026eaa
SETHI06 <puneetsethi32@gmail.com>
2026-07-23 19:27:05 +0530

    Time: 35 ms (86.8%), Space: 45.6 MB (40.85%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,15 +3,15 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map <int, int> preSumMap;
 
-        // preSumMap[0] = 1;  
+        preSumMap[0] = 1;  
         int prefix = 0;
         int count = 0;
 
         for (int i = 0; i < nums.size(); i++){
             prefix += nums[i];
-            if (prefix == k){
-                count++;
-            }
+            // if (prefix == k){
+            //     count++;
+            // }
 
             int rem = prefix - k;
             if (preSumMap.find(rem) != preSumMap.end()){
